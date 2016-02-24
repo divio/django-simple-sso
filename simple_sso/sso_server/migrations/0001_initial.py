08c7174e2323bab96f66e989fd45b6a28e6fd8e9
Kim Thoenen <kim@smuzey.ch>
2016-02-24 16:55:03 +0100

    fix deconstructible and initial migration
    for real this time

--- simple_sso/sso_server/migrations/0001_initial.py
+++ simple_sso/sso_server/migrations/0001_initial.py
@@ -19,16 +19,16 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(unique=True, max_length=100)),
-                ('private_key', models.CharField(default=simple_sso.sso_server.models.SecretKeyGenerator(b'private_key'), unique=True, max_length=64)),
-                ('public_key', models.CharField(default=simple_sso.sso_server.models.SecretKeyGenerator(b'public_key'), unique=True, max_length=64)),
+                ('private_key', models.CharField(default=simple_sso.sso_server.models.ConsumerSecretKeyGenerator(b'private_key'), unique=True, max_length=64)),
+                ('public_key', models.CharField(default=simple_sso.sso_server.models.ConsumerSecretKeyGenerator(b'public_key'), unique=True, max_length=64)),
             ],
         ),
         migrations.CreateModel(
             name='Token',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('request_token', models.CharField(default=simple_sso.sso_server.models.SecretKeyGenerator(b'request_token'), unique=True, max_length=64)),
-                ('access_token', models.CharField(default=simple_sso.sso_server.models.SecretKeyGenerator(b'access_token'), unique=True, max_length=64)),
+                ('request_token', models.CharField(default=simple_sso.sso_server.models.TokenSecretKeyGenerator(b'request_token'), unique=True, max_length=64)),
+                ('access_token', models.CharField(default=simple_sso.sso_server.models.TokenSecretKeyGenerator(b'access_token'), unique=True, max_length=64)),
                 ('timestamp', models.DateTimeField(default=datetime.datetime.now)),
                 ('redirect_to', models.CharField(max_length=255)),
                 ('consumer', models.ForeignKey(related_name='tokens', to='sso_server.Consumer')),
