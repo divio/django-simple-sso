e2ed4db34d9261d327888f083961af8a2819d860
Jamie <jamie@cybersmart.co.uk>
2023-02-16 12:12:19 +0000

    Singleton pattern #52  patch

--- simple_sso/sso_server/server.py
+++ simple_sso/sso_server/server.py
@@ -139,7 +139,8 @@ class Server:
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
-        self.register_admin()
+        if not admin.site.is_registered(Consumer):
+            self.register_admin()
 
     def register_admin(self):
         admin.site.register(Consumer, self.client_admin)
